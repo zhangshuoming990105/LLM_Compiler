6b40fb87f67edbeaecbfd6dadfb661e4a19ff738
zhangshuoming990105 <zhangshuoming17@mails.ucas.ac.cn>
2024-09-09 15:39:53 +0800

    save

--- sandbox/csmith_workspace/gen.sh
+++ sandbox/csmith_workspace/gen.sh
@@ -1,1 +1,7 @@
-csmith --no-pointers --no-volatile-pointers --no-builtins --no-volatiles --no-unions --no-checksum --no-jumps --no-bitfields --no-packed-struct --no-safe-math --no-global-variables --max-pointer-depth 1 --max-expr-complexity 2 --max-block-depth 3 --max-block-size 2 --max-funcs 1 > random4.c
+mkdir -p csmith_100
+cd csmith_100
+# gen 100 files
+for i in {1..100}
+do
+    csmith --quiet --concise --no-pointers --no-volatile-pointers --no-builtins --no-volatiles --no-unions --no-checksum --no-jumps --no-bitfields --no-packed-struct --no-safe-math --no-global-variables --max-pointer-depth 1 --max-expr-complexity 2 --max-block-depth 3 --max-block-size 2 --max-funcs 1 > random$i.c
+done
--- sandbox/longfunction_workspace/token_count.py
+++ sandbox/longfunction_workspace/token_count.py
@@ -1,4 +1,5 @@
 import tiktoken
+import os
 
 def num_token_from_string(
     string: str, encoding_name: str = "gpt-3.5-turbo-0613"
@@ -7,9 +8,13 @@ def num_token_from_string(
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
-c_code = open("/root/workspace/LLM_Compiler/sandbox/longfunction_workspace/long2_it10.c", "r").read()
-asm_code = open("/root/workspace/LLM_Compiler/sandbox/longfunction_workspace/llm_annotated_long2_it10_llm.s", "r").read()
-c_tokens = num_token_from_string(c_code)
-asm_tokens = num_token_from_string(asm_code)
-print(c_tokens)
-print(asm_tokens)
+dir = "/root/workspace/LLM_Compiler/sandbox/longfunction_workspace/"
+for file in os.listdir(dir):
+    if file.endswith(".c"):
+        c_code = open(dir + file, "r").read()
+        c_tokens = num_token_from_string(c_code)
+        print(f"{file}: {c_tokens}")
+    if file.endswith(".s"):
+        asm_code = open(dir + file, "r").read()
+        asm_tokens = num_token_from_string(asm_code)
+        print(f"{file}: {asm_tokens}")
