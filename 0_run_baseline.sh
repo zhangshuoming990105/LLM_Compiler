c18d55d3e2c5f4861fe7977a4f3e5a3887d63d16
zhangshuoming990105 <zhangshuoming17@mails.ucas.ac.cn>
2024-09-16 15:18:05 +0800

    save

--- 0_run_baseline.sh
+++ 0_run_baseline.sh
@@ -1,3 +1,3 @@
 # bash baseline.sh modelname
 # modelname: the name of the model to be used
-python main.py --model $1 --temperature 0.0 --begin_id 0 --end_id 100 --use_mask False
+python main.py --model $1 --temperature 0.0 --begin_id 0 --end_id $2 --use_mask True --mask_stage baseline
